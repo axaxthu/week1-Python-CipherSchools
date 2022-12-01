ce98174bdb0cd825b6604802458f93c929002d2d
ANANTHAPADMANABHAN <ananthasayanam2008@gmail.com>
2022-12-01 23:20:23 +0530

    IF

---
+++ class-s5-ifelse/if.py
@@ -0,0 +1,13 @@
+a=True
+if True:
+    print("the value is true")
+print("end")
+
+
+a=5
+if a==3:
+    print("this value is 3")
+elif a==5:
+    print("this value is 5")
+else:
+    print("this value is not 3 or 5")    
