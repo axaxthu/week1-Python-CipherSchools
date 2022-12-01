bdebc92f6df8a12f158b9277f9e735def52f596b
ANANTHAPADMANABHAN <ananthasayanam2008@gmail.com>
2022-12-01 23:21:49 +0530

    METHODS

---
+++ class-s6-loop/breakcontpass.py
@@ -0,0 +1,18 @@
+for i in range(5):
+    if i==3:
+        continue
+    else:
+        print(i)
+
+for i in range(10):
+    if i==8:
+        break
+    else:
+        print(i)
+
+for i in range(5):
+    print(i)
+    if i==3:
+        break
+    
+
