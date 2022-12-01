e850a34dd202e96f28563104d6ef1c9a08a8c448
ANANTHAPADMANABHAN <ananthasayanam2008@gmail.com>
2022-12-01 23:18:53 +0530

    LOGIC

---
+++ class-s4-expression/logi.py
@@ -0,0 +1,12 @@
+print(True and False)
+print(True or False)
+print(True and 1)
+print(1 and 0)
+print(1 and 5)
+
+#true or b=true
+#false or b=b
+#true and b= b
+#false and b=false
+
+print(bool({1,2,3}))
